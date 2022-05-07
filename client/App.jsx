846f25b2cfb4ac091fa9e1a6dc1953d61295ea68
Max Cook <maxwellcook@Maxs-MBP.hsd1.or.comcast.net>
2022-05-07 11:42:57 -0700

    Boilerplate

---
+++ client/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Login from './routes/Login.jsx';
+// import Nav from './components/Nav.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        IM IN REACT!!
+      </div>
+    );
+  }
+}
+export default App;
