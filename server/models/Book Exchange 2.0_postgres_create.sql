3fa4da51fcc161ff2fe9a49ccf9e7f030124b236
Hannah Santoyo <hannahrichardxo@gmail.com>
2022-05-11 12:01:41 -0700

    added sql database schema

--- server/models/Book Exchange 2.0_postgres_create.sql
+++ server/models/Book Exchange 2.0_postgres_create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
 	"user_id" serial PRIMARY KEY NOT NULL,
-	"username" varchar NOT NULL,
+	"username" varchar NOT NULL UNIQUE,
 	"password" varchar NOT NULL,
 	"email" varchar NOT NULL,
 	"phone" varchar NOT NULL,
