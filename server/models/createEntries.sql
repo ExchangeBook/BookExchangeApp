3635efb1da8455a34d99dbe3eb6a4d43314e771f
James Edwards <j.olden.edwards@gmail.com>
2022-05-11 14:42:09 -0700

    Adding the .sql files that create the tables and add dummy data

---
+++ server/models/createEntries.sql
@@ -0,0 +1,30 @@
+INSERT INTO users(email) 
+VALUES
+  ('james@aol.com'),
+  ('ken@aol.com'),
+  ('li@aol.com'),
+  ('tristen@aol.com'),
+  ('alex@aol.com')
+;
+
+INSERT INTO books(title, author, genre, user_id)
+VALUES
+  ('Harry Potter 1', 'JK Rowling', 'fantasy', 1),
+  ('The Big Short', 'Michael Lewis', 'non-fiction', 2),
+  ('Brothers Karamazov', 'Fyodor Dosteovsky', 'historical fiction', 3),
+  ('book1', 'author 1', 'genre 1', 1),
+  ('book1', 'author 1', 'genre 1', 2),
+  ('book2', 'author 2', 'genre 2', 3),
+  ('book4', 'author 4', 'genre 4', 4),
+  ('book5', 'author 5', 'genre 1', 5),
+  ('book6', 'author 6', 'genre 1', 1),
+  ('book6', 'author 7', 'genre 2', 2),
+  ('book7', 'author 8', 'genre 3', 3),
+  ('book8', 'author 9', 'genre 1', 4),
+  ('book9', 'author 10', 'genre 1', 5),
+  ('book10', 'author 11', 'genre 2', 1),
+  ('book11', 'author 12', 'genre 3', 2),
+  ('book12', 'author 13', 'genre 1', 3),
+  ('book13', 'author 14', 'genre 2', 4),
+  ('book14', 'author 15', 'genre 3', 5)
+;
---
+++ server/models/createNewTables.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS users, books, old_books, wishlist, user_library;
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS books (
+  id SERIAL PRIMARY KEY,
+  isbn VARCHAR(30),
+  title VARCHAR(500) NOT NULL, 
+  author VARCHAR(500) NOT NULL,
+  genre VARCHAR(50) NOT NULL,
+  img_URL varchar(200),
+  user_id INTEGER references users(id) 
+);
+
+CREATE TABLE IF NOT EXISTS wishlist (
+  user_id INTEGER references users(id),
+  book_id INTEGER references books(id),
+  PRIMARY KEY(user_id, book_id)
+);
+
+CREATE TABLE IF NOT EXISTS user_library (
+  user_id INTEGER references users(id),
+  book_id INTEGER references books(id),
+  PRIMARY KEY(user_id, book_id)
+);
